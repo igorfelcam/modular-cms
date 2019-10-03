97e6ff681b235b8b9cc76a7cf05be7b901774960
Igor Camargo <igorfelcam@hotmail.com>
2019-10-02 22:42:41 -0300

    create base model

---
+++ app/Http/Controllers/TypeModuleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TypeModule;
+use Illuminate\Http\Request;
+
+class TypeModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TypeModule  $typeModule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypeModule $typeModule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TypeModule  $typeModule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TypeModule $typeModule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TypeModule  $typeModule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypeModule $typeModule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TypeModule  $typeModule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypeModule $typeModule)
+    {
+        //
+    }
+}
---
+++ app/TypeModule.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeModule extends Model
+{
+    //
+}
